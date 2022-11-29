9aa750902663a9d9d0ab1f15ff89d32586d40578
giannurwana19 <giannurwana19@gmail.com>
2022-11-29 17:00:14 +0700

    sample get token pada php-jwt

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('get-token', function () {
+    $access_token = JWT::encode([
+        'nama' => 'Gian Nurwana',
+        'email' => 'gian@gmail.com',
+        'exp' => time() + 15 // 15 detik
+    ], env('APP_ACCESS_TOKEN'), 'HS256');
+
+    $refresh_token = JWT::encode([
+        'nama' => 'Gian Nurwana',
+        'email' => 'gian@gmail.com',
+        'exp' => time() + 20 // 20 detik
+    ], env('APP_REFRESH_TOKEN'), 'HS256');
+
+    setcookie('refreshToken', $refresh_token, time() + 20, '', '', false, true);
+
+    return response()->json([
+        'access_token' => $access_token
+    ]);
 });
